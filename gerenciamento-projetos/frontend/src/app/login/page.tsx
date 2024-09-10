4c6511b51dca003e16601ea89a2cde58fc340830
Matheus Estevão <matheus935@gmail.com>
2024-09-09 21:07:39 -0300

    feat: front, componentes e paginas de Login e Cadastro de usuarios

---
+++ gerenciamento-projetos/frontend/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import SignInCard from "../components/SignInCard";
+export default function login() {
+return(   
+<Box display={'flex'} justifyContent={'center'} alignItems={'center'} height={'100vh'}>
+    <SignInCard />
+</Box>
+)}
---
+++ gerenciamento-projetos/frontend/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import SignUpCard from "../components/SignUpCard";
+
+export default function Register() {
+   return ( 
+    <Box display={'flex'} justifyContent={'center'} alignItems={'center'} height={'100vh'}>
+        <SignUpCard/>
+    </Box>
+   )}
